17996b783124c9452979fb41287d31cbf7c8bc0b
Lisa Fung <lisafung710@gmail.com>
2024-09-29 09:13:15 -0700

    attempt to connect flask with react with get

--- smeshboard/smeshboard.py
+++ smeshboard/smeshboard.py
@@ -5,9 +5,13 @@ app = Flask(__name__)
 
 # Route decorator indicates which route should trigger function home()py
 @app.route('/')
+def home():
+    return "<p>Welcome to SMeshboard!</p>"
+
+@app.get('/show-data')
 def show_data():
-    # return "<p>Hello, Flask!</p>"
-    return render_template('show_data.html')
+    data = {"message": render_template('show_data.html')}
+    return jsonify(data)
 
 if __name__ == '__main__':
     app.run(debug=True)
--- smeshboard/smeshboard/src/App.js
+++ smeshboard/smeshboard/src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {apiResponse: ""};
+  }
+  callAPI() {
+    fetch("/show-data")
+      .then(res => res.json())
+      .then(res => this.setState({ apiResponse: res }));
+  }
+  componentDidMount() {
+    this.callAPI();
+  }
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <p>{this.state.apiResponse}</p>
+        </header>
+      </div>
+    );
+  }
 }
-
 export default App;
