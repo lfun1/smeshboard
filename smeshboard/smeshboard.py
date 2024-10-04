5c6371dab2933a675506e46197f2dbbbfdbd4292
Lisa Fung <lisafung710@gmail.com>
2024-10-03 22:09:46 -0700

    clean up code

--- smeshboard/smeshboard.py
+++ smeshboard/smeshboard.py
@@ -1,21 +1,17 @@
-from flask import Flask, send_file, render_template
+from flask import Flask, send_file
 from flask_cors import CORS
 import os
 
-# Create instance of Flask class
 app = Flask(__name__)
-CORS(app) # enable CORS (Cross-Origin Resource Sharing) for all routes
+CORS(app) # enable CORS for React to access Flask via HTTP
 
-# Route decorator indicates which route should trigger function home()py
 @app.route('/')
 def home():
     return "<p>Welcome to SMeshboard's Flask backend server!</p>"
 
 @app.get('/get-data')
 def send_data():
-    # data = {"message": render_template('show_data.html')}
     data_path = os.path.join(app.root_path, 'static', '4004_pmsa003i.csv')
-    filepath = {"filepath" : data_path}
     return send_file(data_path, mimetype='text/csv')
 
 if __name__ == '__main__':
