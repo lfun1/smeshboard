171624bbcedb2bd12c37535323dc03eb2d46ecd2
Lisa Fung <lisafung710@gmail.com>
2024-10-03 22:07:51 -0700

    connect react with flask to show data csv

--- smeshboard/smeshboard.py
+++ smeshboard/smeshboard.py
@@ -1,17 +1,22 @@
-from flask import Flask, render_template
+from flask import Flask, send_file, render_template
+from flask_cors import CORS
+import os
 
 # Create instance of Flask class
 app = Flask(__name__)
+CORS(app) # enable CORS (Cross-Origin Resource Sharing) for all routes
 
 # Route decorator indicates which route should trigger function home()py
 @app.route('/')
 def home():
-    return "<p>Welcome to SMeshboard!</p>"
+    return "<p>Welcome to SMeshboard's Flask backend server!</p>"
 
-@app.get('/show-data')
-def show_data():
-    data = {"message": render_template('show_data.html')}
-    return jsonify(data)
+@app.get('/get-data')
+def send_data():
+    # data = {"message": render_template('show_data.html')}
+    data_path = os.path.join(app.root_path, 'static', '4004_pmsa003i.csv')
+    filepath = {"filepath" : data_path}
+    return send_file(data_path, mimetype='text/csv')
 
 if __name__ == '__main__':
     app.run(debug=True)
--- smeshboard/smeshboard/src/App.js
+++ smeshboard/smeshboard/src/App.js
@@ -1,24 +1,16 @@
 import React from "react";
+import './styles.css';
+
+import CsvViewer from "./CsvViewer";
 
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {apiResponse: ""};
-  }
-  callAPI() {
-    fetch("/show-data")
-      .then(res => res.json())
-      .then(res => this.setState({ apiResponse: res }));
-  }
-  componentDidMount() {
-    this.callAPI();
   }
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <p>{this.state.apiResponse}</p>
-        </header>
+      <div>
+        <CsvViewer />
       </div>
     );
   }
